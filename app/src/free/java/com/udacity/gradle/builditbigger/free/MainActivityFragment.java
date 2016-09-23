5c25f8305d4d3c7c4c60b0fca08836a0be71f200
Hoa Nguyen <nvh0412@gmail.com>
2016-09-23 18:19:31 +0700

    [f] fisnished iterestialAD

--- app/src/free/java/com/udacity/gradle/builditbigger/free/MainActivityFragment.java
+++ app/src/free/java/com/udacity/gradle/builditbigger/free/MainActivityFragment.java
@@ -1,6 +1,7 @@
 package com.udacity.gradle.builditbigger.free;
 
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
@@ -8,16 +9,18 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 
+import com.google.android.gms.ads.AdListener;
 import com.google.android.gms.ads.AdRequest;
-import com.google.android.gms.ads.AdView;
+import com.google.android.gms.ads.InterstitialAd;
 import com.udacity.gradle.EndpointAsyncTask;
 import com.udacity.gradle.builditbigger.R;
+import com.vagabond.ajokelib.AJokeActivity;
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class MainActivityFragment extends Fragment {
-
+  InterstitialAd mInterstitialAd;
 
   public MainActivityFragment() {
     // Required empty public constructor
@@ -28,23 +31,52 @@ public class MainActivityFragment extends Fragment {
                            Bundle savedInstanceState) {
     View root = inflater.inflate(R.layout.fragment_main, container, false);
 
-    AdView mAdView = (AdView) root.findViewById(R.id.adView);
-    // Create an ad request. Check logcat output for the hashed device ID to
-    // get test ads on a physical device. e.g.
-    // "Use AdRequest.Builder.addTestDevice("ABCDEF012345") to get test ads on this device."
-    AdRequest adRequest = new AdRequest.Builder()
-      .addTestDevice(AdRequest.DEVICE_ID_EMULATOR)
-      .build();
-    mAdView.loadAd(adRequest);
+    mInterstitialAd = new InterstitialAd(getActivity());
+    mInterstitialAd.setAdUnitId(getString(R.string.banner_ad_unit_id));
+
+    mInterstitialAd.setAdListener(new AdListener() {
+      @Override
+      public void onAdClosed() {
+        requestNewInterstitial();
+        beginLoadJokeMessage();
+      }
+    });
+
+    requestNewInterstitial();
 
     Button jokeBtn = (Button) root.findViewById(R.id.joke_button);
     jokeBtn.setOnClickListener(new View.OnClickListener() {
       @Override
       public void onClick(View view) {
-        new EndpointAsyncTask().execute(getActivity());
+        if (mInterstitialAd.isLoaded()) {
+          mInterstitialAd.show();
+        } else {
+          beginLoadJokeMessage();
+        }
       }
     });
 
     return root;
+  }
+
+  private void beginLoadJokeMessage() {
+    EndpointAsyncTask task = new EndpointAsyncTask();
+    task.setmListener(new EndpointAsyncTask.EndpointAsyncTaskListener() {
+      @Override
+      public void onComplete(String result, Exception e) {
+        Intent intent = new Intent(getActivity(), AJokeActivity.class);
+        intent.putExtra(AJokeActivity.JOKE, result);
+        getActivity().startActivity(intent);
+      }
+    });
+    task.execute(getActivity());
+  }
+
+  private void requestNewInterstitial() {
+    AdRequest adRequest = new AdRequest.Builder()
+      .addTestDevice(AdRequest.DEVICE_ID_EMULATOR)
+      .build();
+
+    mInterstitialAd.loadAd(adRequest);
   }
 }
